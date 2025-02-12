a809a1334e71f2d831f92846a9ba71d7b868c6d6
Andrew Duncan <arduncan@bigpond.com>
2025-02-12 18:23:38 +1100

    double buffering and wait for sync

--- libdrmfb32/framebuffer8880.cxx
+++ libdrmfb32/framebuffer8880.cxx
@@ -217,42 +217,14 @@ findDrmDeviceWithConnector(
 
 //-------------------------------------------------------------------------
 
-}
-
-//=========================================================================
-
-fb32::FrameBuffer8880::FrameBuffer8880(
-    const std::string& device,
+std::string
+findDrmDevice(
     uint32_t connectorId)
-:
-    m_width{0},
-    m_height{0},
-    m_length{0},
-    m_lineLengthPixels{0},
-    m_fd{},
-    m_fbp{nullptr},
-    m_fbId{0},
-    m_fbHandle{0},
-    m_connectorId{connectorId},
-    m_crtcId{0},
-    m_mode{},
-    m_originalCrtc(nullptr, [](drmModeCrtc*){})
 {
-    std::string card{device};
-
-    if (card.empty())
-    {
-        if (connectorId)
-        {
-            card = findDrmDeviceWithConnector(connectorId);
-        }
-        else
-        {
-            card = findDrmDevice();
-        }
-    }
-
-    if (card.empty())
+    std::string device = (connectorId) ?
+                         findDrmDeviceWithConnector(connectorId) :
+                         findDrmDevice();
+    if (device.empty())
     {
         if (connectorId)
         {
@@ -270,11 +242,38 @@ fb32::FrameBuffer8880::FrameBuffer8880(
         }
     }
 
+    return device;
+}
+
+//-------------------------------------------------------------------------
+
+}
+
+//=========================================================================
+
+fb32::FrameBuffer8880::FrameBuffer8880(
+    const std::string& device,
+    uint32_t connectorId)
+:
+    m_width{0},
+    m_height{0},
+    m_fd{},
+    m_dbs{},
+    m_dbFront{0},
+    m_dbBack{1},
+    m_connectorId{connectorId},
+    m_crtcId{0},
+    m_mode{},
+    m_originalCrtc(nullptr, [](drmModeCrtc*){})
+{
+    std::string card{device};
+
+    if (card.empty())
+    {
+        card = findDrmDevice(connectorId);
+    }
+
     m_fd = FileDescriptor{::open(card.c_str(), O_RDWR)};
-
-    //---------------------------------------------------------------------
-
-    drmSetMaster(m_fd.fd());
 
     //---------------------------------------------------------------------
 
@@ -287,6 +286,213 @@ fb32::FrameBuffer8880::FrameBuffer8880(
 
     //---------------------------------------------------------------------
 
+    findResources(connectorId);
+    drmSetMaster(m_fd.fd());
+
+    //---------------------------------------------------------------------
+
+    createDumbBuffer(m_dbFront);
+    createDumbBuffer(m_dbBack);
+
+    update();
+}
+
+//-------------------------------------------------------------------------
+
+fb32::FrameBuffer8880::~FrameBuffer8880()
+{
+    destroyDumbBuffer(m_dbBack);
+    destroyDumbBuffer(m_dbFront);
+
+    drmModeSetCrtc(m_fd.fd(),
+                   m_originalCrtc->crtc_id,
+                   m_originalCrtc->buffer_id,
+                   m_originalCrtc->x,
+                   m_originalCrtc->y,
+                   &m_connectorId,
+                   1,
+                   &(m_originalCrtc->mode));
+
+    if (drmIsMaster(m_fd.fd()))
+    {
+        drmDropMaster(m_fd.fd());
+    }
+}
+
+//-------------------------------------------------------------------------
+
+std::span<uint32_t>
+fb32::FrameBuffer8880::getBuffer() noexcept
+{
+    auto& dbb = m_dbs[m_dbBack];
+    return {dbb.m_fbp, getBufferSize()};
+}
+
+//-------------------------------------------------------------------------
+
+std::span<const uint32_t>
+fb32::FrameBuffer8880::getBuffer() const noexcept
+{
+    auto& dbb = m_dbs[m_dbBack];
+    return {dbb.m_fbp, getBufferSize()};
+}
+
+//-------------------------------------------------------------------------
+
+size_t
+fb32::FrameBuffer8880::getBufferSize() const noexcept
+{
+    auto& dbb = m_dbs[m_dbBack];
+    return dbb.m_lineLengthPixels * m_height;
+}
+
+//-------------------------------------------------------------------------
+
+size_t
+fb32::FrameBuffer8880::offset(
+    const Interface8880Point& p) const noexcept
+{
+    auto& dbb = m_dbs[m_dbBack];
+    return p.x() + p.y() * dbb.m_lineLengthPixels;
+}
+
+//-------------------------------------------------------------------------
+
+void
+fb32::FrameBuffer8880::update()
+{
+    std::swap(m_dbFront, m_dbBack);
+    auto& dbf = m_dbs[m_dbFront];
+
+    drmVBlank vbl;
+    vbl.request.type = DRM_VBLANK_RELATIVE;
+    vbl.request.sequence = 1;
+    drmWaitVBlank(m_fd.fd(), &vbl);
+
+    auto setCrtcResult = drmModeSetCrtc(m_fd.fd(),
+                                        m_crtcId,
+                                        dbf.m_fbId,
+                                        0,
+                                        0,
+                                        &m_connectorId,
+                                        1,
+                                        &m_mode);
+    if (setCrtcResult < 0)
+    {
+        throw std::system_error(errno,
+                                std::system_category(),
+                                "unable to set crtc with frame buffer");
+    }
+}
+
+//-------------------------------------------------------------------------
+
+void
+fb32::FrameBuffer8880::createDumbBuffer(
+    int index)
+{
+    auto& db = m_dbs[index];
+
+    drm_mode_create_dumb dmcb =
+    {
+        .height = m_mode.vdisplay,
+        .width = m_mode.hdisplay,
+        .bpp = 32,
+        .flags = 0,
+        .handle = 0,
+        .pitch = 0,
+        .size = 0
+    };
+
+    if (drmIoctl(m_fd.fd(), DRM_IOCTL_MODE_CREATE_DUMB, &dmcb) < 0)
+    {
+        throw std::system_error{errno,
+                                std::system_category(),
+                                "Cannot create a DRM dumb buffer"};
+    }
+
+    //---------------------------------------------------------------------
+
+    db.m_length = dmcb.size;
+    db.m_lineLengthPixels = dmcb.pitch / c_bytesPerPixel;
+    db.m_fbHandle = dmcb.handle;
+
+    uint32_t handles[4] = { dmcb.handle };
+    uint32_t strides[4] = { dmcb.pitch };
+    uint32_t offsets[4] = { 0 };
+
+    if (drmModeAddFB2(
+            m_fd.fd(),
+            m_mode.hdisplay,
+            m_mode.vdisplay,
+            DRM_FORMAT_XRGB8888,
+            handles,
+            strides,
+            offsets,
+            &db.m_fbId,
+            0) < 0)
+    {
+        throw std::system_error{errno,
+                                std::system_category(),
+                                "Cannot add frame buffer"};
+    }
+
+    //---------------------------------------------------------------------
+
+    drm_mode_map_dumb dmmd =
+    {
+        .handle = db.m_fbHandle
+    };
+
+    if (drmIoctl(m_fd.fd(), DRM_IOCTL_MODE_MAP_DUMB, &dmmd) < 0)
+    {
+        throw std::system_error{errno,
+                                std::system_category(),
+                                "Cannot map dumb buffer"};
+    }
+
+    void* fbp = mmap(0,
+                     db.m_length,
+                     PROT_READ | PROT_WRITE,
+                     MAP_SHARED,
+                     m_fd.fd(),
+                     dmmd.offset);
+
+    if (fbp == MAP_FAILED)
+    {
+        throw std::system_error(errno,
+                                std::system_category(),
+                                "mapping framebuffer device to memory");
+    }
+
+    db.m_fbp = static_cast<uint32_t*>(fbp);
+}
+
+//-------------------------------------------------------------------------
+
+void
+fb32::FrameBuffer8880::destroyDumbBuffer(
+    int index)
+{
+    auto& db = m_dbs[index];
+
+    ::munmap(db.m_fbp, db.m_length);
+    drmModeRmFB(m_fd.fd(), db.m_fbId);
+
+    drm_mode_destroy_dumb dmdd =
+    {
+        .handle = db.m_fbHandle
+    };
+
+    drmIoctl(m_fd.fd(), DRM_IOCTL_MODE_DESTROY_DUMB, &dmdd);
+}
+
+//-------------------------------------------------------------------------
+
+void
+fb32::FrameBuffer8880::findResources(
+    uint32_t connectorId)
+{
     uint64_t hasDumb;
     if ((drmGetCap(m_fd.fd(), DRM_CAP_DUMB_BUFFER, &hasDumb) < 0) or not hasDumb)
     {
@@ -313,136 +519,11 @@ fb32::FrameBuffer8880::FrameBuffer8880(
     //---------------------------------------------------------------------
 
     m_mode = resource.m_mode;
-
     m_width = m_mode.hdisplay;
     m_height = m_mode.vdisplay;
-
-    drm_mode_create_dumb dmcb =
-    {
-        .height = m_mode.vdisplay,
-        .width = m_mode.hdisplay,
-        .bpp = 32,
-        .flags = 0,
-        .handle = 0,
-        .pitch = 0,
-        .size = 0
-    };
-
-    if (drmIoctl(m_fd.fd(), DRM_IOCTL_MODE_CREATE_DUMB, &dmcb) < 0)
-    {
-        throw std::system_error{errno,
-                                std::system_category(),
-                                "Cannot create a DRM dumb buffer"};
-    }
-
-    //---------------------------------------------------------------------
-
-    m_length = dmcb.size;
-    m_lineLengthPixels = dmcb.pitch / c_bytesPerPixel;
-    m_fbHandle = dmcb.handle;
-
-    uint32_t handles[4] = { dmcb.handle };
-    uint32_t strides[4] = { dmcb.pitch };
-    uint32_t offsets[4] = { 0 };
-
-    if (drmModeAddFB2(
-            m_fd.fd(),
-            m_mode.hdisplay,
-            m_mode.vdisplay,
-            DRM_FORMAT_XRGB8888,
-            handles,
-            strides,
-            offsets,
-            &m_fbId,
-            0) < 0)
-    {
-        throw std::system_error{errno,
-                                std::system_category(),
-                                "Cannot add frame buffer"};
-    }
-
-    //---------------------------------------------------------------------
-
-    drm_mode_map_dumb dmmd =
-    {
-        .handle = m_fbHandle
-    };
-
-    if (drmIoctl(m_fd.fd(), DRM_IOCTL_MODE_MAP_DUMB, &dmmd) < 0)
-    {
-        throw std::system_error{errno,
-                                std::system_category(),
-                                "Cannot map dumb buffer"};
-    }
-
-    void* fbp = mmap(0, m_length, PROT_READ | PROT_WRITE, MAP_SHARED, m_fd.fd(), dmmd.offset);
-
-    if (fbp == MAP_FAILED)
-    {
-        throw std::system_error(errno,
-                                std::system_category(),
-                                "mapping framebuffer device to memory");
-    }
-
-    m_fbp = static_cast<uint32_t*>(fbp);
-
-    //---------------------------------------------------------------------
 
     m_connectorId = resource.m_connectorId;
     m_crtcId = resource.m_crtcId;
     m_originalCrtc = drm::drmModeGetCrtc(m_fd, resource.m_crtcId);
-
-    if (drmModeSetCrtc(m_fd.fd(), resource.m_crtcId, m_fbId, 0, 0, &m_connectorId, 1, &m_mode) < 0)
-    {
-        throw std::system_error(errno,
-                                std::system_category(),
-                                "unable to set crtc with frame buffer");
-    }
-}
-
-//-------------------------------------------------------------------------
-
-fb32::FrameBuffer8880::~FrameBuffer8880()
-{
-    ::munmap(m_fbp, m_length);
-    drmModeRmFB(m_fd.fd(), m_fbId);
-
-    drm_mode_destroy_dumb dmdd =
-    {
-        .handle = m_fbHandle
-    };
-
-    drmIoctl(m_fd.fd(), DRM_IOCTL_MODE_DESTROY_DUMB, &dmdd);
-
-    drmModeSetCrtc(m_fd.fd(),
-                   m_originalCrtc->crtc_id,
-                   m_originalCrtc->buffer_id,
-                   m_originalCrtc->x,
-                   m_originalCrtc->y,
-                   &m_connectorId,
-                   1,
-                   &(m_originalCrtc->mode));
-
-    if (drmIsMaster(m_fd.fd()))
-    {
-        drmDropMaster(m_fd.fd());
-    }
-}
-
-//-------------------------------------------------------------------------
-
-size_t
-fb32::FrameBuffer8880::offset(
-    const Interface8880Point& p) const noexcept
-{
-    return p.x() + p.y() * m_lineLengthPixels;
-}
-
-//-------------------------------------------------------------------------
-
-void
-fb32::FrameBuffer8880::update()
-{
-    drmModeSetCrtc(m_fd.fd(), m_crtcId, m_fbId, 0, 0, &m_connectorId, 1, &m_mode);
 }
 
